2af9a95cd44cf592e1db2b26b04088fdd6959aec
Xavier Dunkley <xavierxv18@gmail.com>
2024-02-11 21:21:09 -0500

    started login from in login component

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import React,{ useRef } from 'react'
+import { Container, Form } from 'react-bootstrap'
 
 export default function Login() {
+    const idRef = useRef()
+
   return (
-    <div>Login</div>
+    <Container>
+        <Form>
+            <Form.Group>
+                <Form.Label>Enter Your Id</Form.Label>
+                    <Form.Control type='text' ref={idRef} required/>
+            </Form.Group>
+        </Form>
+    </Container>
   )
 }
