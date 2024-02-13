b2445a5e28eb6a96818f092c8ac39afb1c32be6a
Xavier Dunkley <xavierxv18@gmail.com>
2024-02-13 17:19:07 -0500

    added conversation and contact button to dashboard

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Tab, Nav } from 'react-bootstrap'
+import { Tab, Nav, Button } from 'react-bootstrap'
 import Conversations from './Conversations'
 import Contacts from './Contacts'
 
@@ -9,6 +9,7 @@ const CONTACTS_KEY = 'contacts'
 
 export default function Sidebar({id}) {
     const [activeKey, setActiveKey]=useState(CONVERSATION_KEY)
+    const conversationsOpen = activeKey ===CONVERSATION_KEY
 
   return (
     <div style={{width: '250px'}} className='d-flex flex-column'>
@@ -21,7 +22,7 @@ export default function Sidebar({id}) {
                     <Nav.Link eventKey={CONTACTS_KEY}>Contacts</Nav.Link>
                 </Nav.Item>
             </Nav>
-            <Tab.Content>
+            <Tab.Content className='border-right overflow-auto flex-grow-1'>
                     <Tab.Pane eventKey={CONVERSATION_KEY}>
                         <Conversations/>
                     </Tab.Pane>
@@ -29,6 +30,10 @@ export default function Sidebar({id}) {
                         <Contacts/>
                     </Tab.Pane>
             </Tab.Content>
+            <div className='p-2 border-top border-right small'>
+                Your Id: <span className='text-muted'>{id}</span>
+            </div>
+            <Button>New {conversationsOpen?'Conversation': 'Contact'}</Button>
         </Tab.Container>
     </div>
   )
