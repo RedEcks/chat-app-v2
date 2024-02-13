ffd502d5e91c207ff2037dbbd1dd7e80deaac676
Xavier Dunkley <xavierxv18@gmail.com>
2024-02-13 14:22:13 -0500

    added conversation and contenttabs to sidebar component

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Tab, Nav } from 'react-bootstrap'
+
+
+const CONVERSATION_KEY = 'conversations'
+const CONTACTS_KEY = 'contacts'
 
 export default function Sidebar({id}) {
+    const [activeKey, setActiveKey]=useState(CONVERSATION_KEY)
+
   return (
-    <div>Sidebar</div>
+    <div style={{width: '250px'}} className='d-flex flex-column'>
+        <Tab.Container activeKey={activeKey} onSelect={setActiveKey}>
+            <Nav variant='tabs' className='justify-content-center'>
+                <Nav.Item>
+                    <Nav.Link eventKey={CONVERSATION_KEY}>Conversations</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey={CONTACTS_KEY}>Contacts</Nav.Link>
+                </Nav.Item>
+            </Nav>
+        </Tab.Container>
+    </div>
   )
 }
